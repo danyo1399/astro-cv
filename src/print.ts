942fab15a08e331b7aafbf2b256a7e2ca932d6fb
danyo <dan@noemail.com>
2022-11-08 15:12:07 +1300

    prettier changes and add logging to print

--- src/print.ts
+++ src/print.ts
@@ -1,25 +1,40 @@
-import puppeteer from 'puppeteer'
-import data from  './cvData';
+import puppeteer from 'puppeteer';
+import data from './cvData';
 import fs from 'fs/promises';
 import path from 'path';
 
-const url = `http://localhost:3000/cv`
+const url = `http://localhost:3000/cv`;
 const outputPath = path.join(__dirname, '../public/cv');
 async function printPDF() {
-  const browser = await puppeteer.launch({ headless: true });
-  
-  for(const key of data.map(x => x.id)) {
-    const page = await browser.newPage();
-    await page.goto(`${url}/${key}/print`, {waitUntil: 'networkidle0'});
-    const pdf = await page.pdf({ format: 'A4', printBackground: true });
-    await fs.mkdir(`${outputPath}/${key}`, {recursive: true});
-    await fs.writeFile(`${outputPath}/${key}/cv.pdf`, pdf);
+  const browser = await puppeteer.launch({
+    headless: true,
+    ignoreHTTPSErrors: true
+  });
 
-    page.close()
+  for (const key of data.map((x) => x.id)) {
+    const page = await browser.newPage();
+    const targetUrl = `${url}/${key}/print`;
+    console.log('navigate to', targetUrl);
+    await page.goto(targetUrl, { waitUntil: 'networkidle0' });
+    console.log('printing');
+    const pdf = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+      margin: { top: 0, bottom: 0, right: 0, left: 0 }
+    });
+    page.close();
+    console.log('document printed');
+    const docPath = `${outputPath}/${key}/cv.pdf`;
+    await fs.mkdir(`${outputPath}/${key}`, { recursive: true });
+    await fs.writeFile(docPath, pdf);
+    console.log('document printed to ', docPath);
   }
-  
-  await browser.close();
-  
-};
+  console.log('Closing browser');
+  try {
+    await browser.close();
+  } catch (error) {
+    console.log('Error closing browser. dont care');
+  }
+}
 
 printPDF();
